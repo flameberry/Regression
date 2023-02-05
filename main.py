9c71c1dafa63af7f2de809fcb499c603fe6161d0
Flameberry <flameberrydev@gmail.com>
2023-02-05 12:06:50 +0530

    Changed theme from blue to dark-blue.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PredictionApp import PredictionApp
 
 if __name__ == '__main__':
     customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-    customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+    customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
     app = PredictionApp()
     app.mainloop()
