874d8357ea796d77edcacf060b7ef7b709b77ba6
Flameberry <flameberrydev@gmail.com>
2023-03-03 12:44:58 +0530

    Added method_selection_widget

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import customtkinter
 from RegressionApp import RegressionApp
+from regression import Regression
+from method_selection_widget import MethodSelectionWidget
 
 if __name__ == '__main__':
     customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
     customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-    app = RegressionApp()  # Possible Parameter: methods=(Regression.NeuralNetwork, Regression.MultipleLinear)
+    # widget = MethodSelectionWidget()
+    # widget.mainloop()
+    # methods = tuple([method for method, var in zip(Regression, widget.get_choices()) if var is True])
+    methods = []
+
+    print('Selected regression methods:', methods)
+
+    app = RegressionApp(methods=methods)  # Possible Parameter: methods=(Regression.NeuralNetwork, Regression.MultipleLinear)
     app.mainloop()
